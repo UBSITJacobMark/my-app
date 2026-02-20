9342a6735871e8a7a67f099739b2b4973f87ddaf
UBSITJacobMark <20244156@s.ubaguio.edu>
2026-02-20 13:01:26 +0800

    Fixed Spacing and Typos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-// 1. Import your new components here
 
 import { NavbarComponent } from './navbar/navbar';
 import { ProfileComponent } from './profile/profile';
@@ -12,10 +11,10 @@ import { RegisterComponent } from './register/register';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Required for the imports array to work
+  standalone: true, 
   imports: [
     RouterOutlet,
-    NavbarComponent,   // 2. Add them to this list
+    NavbarComponent,  
     ProfileComponent,
     RegisterComponent
   ],
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common'; // Fix: Import from @angular/common
 import { UserInteractionComponent } from '../user-interaction/user-interaction';
 
+
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [RouterLink, UserInteractionComponent], // Required for the links in the card footer
-  templateUrl: './home.html'
+  selector: 'app-home', // Added comma
+  standalone: true,     // Added comma
+  imports: [RouterLink, UserInteractionComponent, DatePipe], // Added comma
+  templateUrl: './home.html', // Added comma
+  styleUrl: './home.css'      // No comma needed for the last item
 })
+
 export class HomeComponent {
-  // Application Info
   appTitle: string = 'Welcome to Angular 20';
   userImageUrl: string = 'user-avatar.png';
-  developerName: string = 'Jacob';
-  systemVersion: number = 1.2;
   isRegOpen: boolean = true;
+  isHighlighted: boolean = true;
+  currentDate: Date = new Date();
+
 }
